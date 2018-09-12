dec177c2a2e105d721e6074c8a1519fb091655a0
Drake Rentrop <drakerentrop@gmail.com>
2018-09-12 17:11:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
-
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 # Define your WIN_COMBINATIONS constant
 
 
