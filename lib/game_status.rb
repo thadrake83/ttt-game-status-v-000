1c8c36c3ae955853b17b01aba5c3a350fc8895dc
Drake Rentrop <drakerentrop@gmail.com>
2018-09-12 16:30:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,7 +25,7 @@ def won?(board)
   position_1 = board[win_index_1]
   position_2 = board[win_index_2]
   position_3 = board[win_index_3]
-   if position_1 == "X" && position_ == "X" && position_3 == "X"
+   if position_1 == "X" && position_2 == "X" && position_3 == "X"
      return winning_combination
    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
    return winning_combination
